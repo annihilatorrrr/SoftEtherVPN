95650761fdeed9b21dbaf8cdec78d22441d000e2
Sourcery AI <>
2022-11-02 13:53:08 +0000

    'Refactored by Sourcery'

--- version.py
+++ version.py
@@ -12,15 +12,15 @@ def main():
 		for line in file:
 			if 'VERSION "' in line and '.${BUILD_NUMBER}"' in line:
 				line = line.replace('VERSION "', '')
-				line = line[0 : line.find('.${BUILD_NUMBER}"')].strip()
+				line = line[:line.find('.${BUILD_NUMBER}"')].strip()
 				version = line
 				break
 
 	if version is None:
 		raise Exception('Unable to read version from CMakeLists.txt')
 
-	if len(version) == 0 or not '.' in version:
-			raise Exception('Bad version: "{0}"'.format(version))
+	if len(version) == 0 or '.' not in version:
+		raise Exception('Bad version: "{0}"'.format(version))
 
 	print(version, end = end)
 
